e874b316e9ee88b08c1b85d76324a0e4074d07bc
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-12-25 03:35:50 -0300

    basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log(123)
+import { Client } from 'discord.js'
+
+const app = new Client({ intents: [] })
+
+app.on('ready', (client) => {
+  console.log('started')
+
+  for (const key of client.guilds.cache) {
+    console.log(key)
+  }
+})
+
+app.login(process.env.DISCORD_TOKEN)
